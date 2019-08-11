8bfa37a381b0bc088729ed7837f0a7180bdb2f27
ruanjiqian@globalegrow.com <ruanjiqian@globalegrow.com>
2019-08-11 20:28:29 +0800

    排序算法

--- sort.js
+++ sort.js
@@ -1,76 +1,60 @@
 function quickSort(arr){
-    if(arr.length <= 1){
-        return arr;
-    }
-    var left = [];
-    var right = [];
-    var temp = arr[0];
-    for(var i = 1; i < arr.length; i++){
-        if(arr[i] < temp){
-            left.push(arr[i]);
-        }else {
-            right.push(arr[i]);
-        }
-    }
-    var leftvalue = quickSort(left);
-    var rightvalue = quickSort(right);
-    var result = leftvalue.concat(temp,rightvalue);
-    return result;
+     
 }
 
 
 function selectSort(arr){
-    var arrcopy = arr.slice();
-    for(var i = arrcopy.length; i > 0; i--){
-        var mv = 0;
-        var mi = 0;
-        for(var j = 0; j < arrcopy.length; j++){
-            if(arrcopy[j]>=mv){
-                arrcopy[j] = mv;
-                mi = j;
+    for(let j = 0; j < arr.length - 1; j++){
+        let min = j;
+        for(let i = j + 1; i < arr.length; i++){
+            if(arr[i] < arr[min]){
+                min = i;
             }
         }
-        var lastvalue = arrcopy[arrcopy.length-1];
-        if(lastvalue > mv){
-            arr[mi] = lastvalue;
-            arr[arrcopy.length-1] = mv;
-            arrcopy.splice(arrcopy.length-1,1);
-        }
+        let temp = arr[j];
+        arr[j] = arr[min];
+        arr[min] = temp;
     }
     return arr;
 }
 
 function insetSort(arr){
-    for(var i = 1; i < arr.length; i++){
-        var temp = arr[i];
-        var j = i;
-        while(arr[j-1] > temp && j > 0){
-            arr[j] = arr[j-1];
-            arr[j-1] = temp;
-            j--;
-        } 
-    }
-    return arr;
-}
-
-function bubbleSort(arr){
-    for(var i = 0; i < arr.length; i++){
-        for(var j = 0; j < arr.length-i-1; j++){
-            var temp = arr[j];
-            if(temp > arr[j+1]){
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    for(let i = 1; i < arr.length; i++){
+        let j = i;
+        while(j > 0){
+            if(arr[j] < arr[j-1]){
+                let temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            } else{
+                break;
             }
         }
     }
+    return arr;
+    
+}
+
+function bubbleSort(arr){
+    for(let j = arr.length - 1; j > 0; j --){
+        for(let i = 0; i < j; i++){
+            if(arr[i] > arr[i+1]){
+                var temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+        
     return arr;
 }
 
 
 var arr = [6,4,5,2,1,7,9,8,3];
-// console.log(insetSort(arr));
+// console.log(selectSort(arr));
 // console.log(bubbleSort(arr));
-// console.log(insetSort(arr));
+console.log(insetSort(arr));
 // console.log(quickSort(arr));
 
 
@@ -87,4 +71,4 @@ function sum(){
 }
 
 // sum(1)(2)(3)();  
-sum(1,3,5)(6,7)(8);
+// sum(1,3,5)(6,7)(8);
